edf0438854d7c249e97d5ba14fa67d7048440f69
Vadim Barashkov <vadimdominik2005@gmail.com>
2024-08-28 21:07:04 +0300

    feat: write isUniqueViolationError func
    Write isUniqueViolationError function that help with INSERT and UPDATE operations.

---
+++ internal/database/postgres/postgres.go
@@ -0,0 +1,28 @@
+package postgres
+
+import (
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jmoiron/sqlx"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
+
+const uniqueViolationErrCode = "23505"
+
+func isUniqueViolationError(err error) bool {
+	pgErr, ok := err.(*pgconn.PgError)
+	return ok && pgErr.SQLState() == uniqueViolationErrCode
+}
+
+func New(dsn string) (*sqlx.DB, error) {
+	const op = "db.postgres.New"
+
+	db, err := sqlx.Connect("pgx", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("%s: failed to connect to database: %w", op, err)
+	}
+
+	return db, nil
+}
