28adad6c023b96ba1784a40e42f70f0bb8d07041
Vadim Barashkov <vadimdominik2005@gmail.com>
2024-08-28 18:07:47 +0300

    feat: write Load function
    Write Load function that load environment variables from .env file and parse them into Config struct.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/vadimbarashkov/url-shortener
 
 go 1.23.0
+
+require (
+	github.com/caarlos0/env/v11 v11.2.2
+	github.com/joho/godotenv v1.5.1
+)
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,27 +1,49 @@
 package config
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/caarlos0/env/v11"
+	"github.com/joho/godotenv"
+)
 
 type Config struct {
-	Env string
-	Server
-	Postgres
+	Env      string `env:"ENV" envDefault:"dev"`
+	Server   `envPrefix:"SERVER_"`
+	Postgres `envPrefix:"POSTGRES_"`
+}
+
+func Load(path string) (*Config, error) {
+	const op = "config.Load"
+
+	if err := godotenv.Load(path); err != nil {
+		return nil, fmt.Errorf("%s: failed to load .env file: %w", op, err)
+	}
+
+	var cfg Config
+
+	if err := env.Parse(&cfg); err != nil {
+		return nil, fmt.Errorf("%s: failed to parse envs: %w", op, err)
+	}
+
+	return &cfg, nil
 }
 
 type Server struct {
-	Port         int
-	ReadTimeout  time.Duration
-	WriteTimeout time.Duration
-	IdleTimeout  time.Duration
-	CertFile     string
-	KeyFile      string
+	Port         int           `env:"PORT" envDefault:"8443"`
+	ReadTimeout  time.Duration `env:"READ_TIMEOUT" envDefault:"1s"`
+	WriteTimeout time.Duration `env:"WRITE_TIMEOUT" envDefault:"2s"`
+	IdleTimeout  time.Duration `env:"IDLE_TIMEOUT" envDefault:"1m"`
+	CertFile     string        `env:"CERT_FILE"`
+	KeyFile      string        `env:"KEY_FILE"`
 }
 
 type Postgres struct {
-	User     string
-	Password string
-	Host     string
-	Port     int
-	DB       string
-	SSLMode  string
+	User     string `env:"USER" envDefault:"postgres"`
+	Password string `env:"PASSWORD"`
+	Host     string `env:"HOST" envDefault:"localhost"`
+	Port     int    `env:"PORT" envDefault:"5432"`
+	DB       string `env:"DB"`
+	SSLMode  string `env:"SSLMODE" envDefault:"disable"`
 }
