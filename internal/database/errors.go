faa7a29e2aa7fe6e3edc89f1f84ca581751a5830
Vadim Barashkov <vadimdominik2005@gmail.com>
2024-08-29 15:22:33 +0300

    docs: write documentation for database package

--- internal/database/errors.go
+++ internal/database/errors.go
@@ -3,6 +3,10 @@ package database
 import "errors"
 
 var (
+	// ErrShortCodeExists is returned when an attempt is made to create
+	// a new shortened URL with a short code that already exists.
 	ErrShortCodeExists = errors.New("short code exists")
-	ErrURLNotFound     = errors.New("url not found")
+	// ErrURLNotFound is returned when an attempt is made to retrieve
+	// a URL using a short code that doesn't exist.
+	ErrURLNotFound = errors.New("url not found")
 )
