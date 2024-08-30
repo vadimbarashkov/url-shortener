ff50defa7a7a2f6ffc3f6e82b39a19e85d4bd743
Vadim Barashkov <vadimdominik2005@gmail.com>
2024-08-30 14:58:30 +0300

    test: write unit test for URLService.ShortenURL method

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -2,10 +2,16 @@ package service
 
 import (
 	"context"
+	"errors"
+	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
+	"github.com/vadimbarashkov/url-shortener/internal/database"
 	"github.com/vadimbarashkov/url-shortener/internal/models"
 )
+
+var errUnknown = errors.New("unknown error")
 
 type MockURLRepository struct {
 	mock.Mock
@@ -38,4 +44,80 @@ func (r *MockURLRepository) GetStats(ctx context.Context, shortCode string) (*mo
 	args := r.Called(ctx, shortCode)
 	url, _ := args.Get(0).(*models.URL)
 	return url, args.Error(1)
+}
+
+func setupURLSerive(t testing.TB, shortCodeLength int) (*URLService, *MockURLRepository) {
+	t.Helper()
+
+	mockRepo := new(MockURLRepository)
+	svc := NewURLService(mockRepo, shortCodeLength)
+
+	return svc, mockRepo
+}
+
+func TestURLService_ShortenURL(t *testing.T) {
+	t.Run("short code generation error", func(t *testing.T) {
+		svc, _ := setupURLSerive(t, -1)
+
+		url, err := svc.ShortenURL(context.TODO(), "https://example.com")
+
+		assert.Error(t, err)
+		assert.Nil(t, url)
+	})
+
+	t.Run("maximum retries error", func(t *testing.T) {
+		svc, mockRepo := setupURLSerive(t, 8)
+
+		mockRepo.On("Create", mock.Anything, mock.Anything, mock.Anything).
+			Times(5).
+			Return(nil, database.ErrShortCodeExists)
+
+		url, err := svc.ShortenURL(context.TODO(), "https://example.com")
+
+		assert.Error(t, err)
+		assert.ErrorIs(t, err, ErrMaxRetriesExceeded)
+		assert.Nil(t, url)
+		mockRepo.AssertExpectations(t)
+		mockRepo.AssertNumberOfCalls(t, "Create", 5)
+	})
+
+	t.Run("unknown error", func(t *testing.T) {
+		svc, mockRepo := setupURLSerive(t, 8)
+
+		mockRepo.On("Create", mock.Anything, mock.Anything, mock.Anything).
+			Times(1).
+			Return(nil, errUnknown)
+
+		url, err := svc.ShortenURL(context.TODO(), "https://example.com")
+
+		assert.Error(t, err)
+		assert.ErrorIs(t, err, errUnknown)
+		assert.Nil(t, url)
+		mockRepo.AssertExpectations(t)
+		mockRepo.AssertNumberOfCalls(t, "Create", 1)
+	})
+
+	t.Run("success", func(t *testing.T) {
+		svc, mockRepo := setupURLSerive(t, 8)
+
+		mockRepo.On("Create", mock.Anything, mock.Anything, mock.Anything).
+			Times(1).
+			Return(&models.URL{
+				ShortCode:   mock.Anything,
+				OriginalURL: "https://example.com",
+			}, nil)
+
+		wantURL := models.URL{
+			ShortCode:   mock.Anything,
+			OriginalURL: "https://example.com",
+		}
+
+		url, err := svc.ShortenURL(context.TODO(), "https://example.com")
+
+		assert.NoError(t, err)
+		assert.NotNil(t, url)
+		assert.Equal(t, wantURL, *url)
+		mockRepo.AssertExpectations(t)
+		mockRepo.AssertNumberOfCalls(t, "Create", 1)
+	})
 }
