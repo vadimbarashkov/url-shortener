70bb6d127e6fe94cfeae7b760cf9c630f8ba9b51
Vadim Barashkov <vadimdominik2005@gmail.com>
2024-08-30 10:10:37 +0300

    feat: write URLService.ShortenURL method
    Write URLService.ShortenURL method that shorten long URL.

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,9 +2,16 @@ package service
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
+	"github.com/vadimbarashkov/url-shortener/internal/database"
 	"github.com/vadimbarashkov/url-shortener/internal/models"
+
+	gonanoid "github.com/matoous/go-nanoid/v2"
 )
+
+var ErrMaxRetriesExceeded = errors.New("maximum retries exceeded for generating short code")
 
 type URLRepository interface {
 	Create(ctx context.Context, shortCode, originalURL string) (*models.URL, error)
@@ -24,4 +31,29 @@ func NewURLService(repo URLRepository, shortCodeLength int) *URLService {
 		repo:            repo,
 		shortCodeLength: shortCodeLength,
 	}
+}
+
+func (s *URLService) ShortenURL(ctx context.Context, originalURL string) (*models.URL, error) {
+	const op = "service.URLService.ShortenURL"
+	const maxRetries = 5
+
+	for i := 0; i < maxRetries; i++ {
+		shortCode, err := gonanoid.New(s.shortCodeLength)
+		if err != nil {
+			return nil, fmt.Errorf("%s: failed to generate short code: %w", op, err)
+		}
+
+		url, err := s.repo.Create(ctx, shortCode, originalURL)
+		if err != nil {
+			if errors.Is(err, database.ErrShortCodeExists) {
+				continue
+			}
+
+			return nil, fmt.Errorf("%s: failed to shorten url: %w", op, err)
+		}
+
+		return url, nil
+	}
+
+	return nil, fmt.Errorf("%s: %w", op, ErrMaxRetriesExceeded)
 }
