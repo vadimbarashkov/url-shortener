54c7d129be28142c641ceca923d8a5f2287eba45
Vadim Barashkov <vadimdominik2005@gmail.com>
2024-09-06 04:04:01 +0300

    feat: define Middleware type and write ChainMiddleware func

---
+++ pkg/middleware/middleware.go
@@ -0,0 +1,12 @@
+package middleware
+
+import "net/http"
+
+type Middleware func(next http.Handler) http.Handler
+
+func ChainMiddleware(h http.Handler, middlewares ...Middleware) http.Handler {
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		h = middlewares[i](h)
+	}
+	return h
+}
