70a95ceda9ed024eb986cf3688f2bbeed0d713ff
Vadim Barashkov <vadimdominik2005@gmail.com>
2024-09-11 13:06:24 +0300

    feat: write entrypoint for server
    Write entrypoint for server that implement graceful shutdown.

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,5 +1,77 @@
 package main
 
-func main() {
+import (
+	"context"
+	"errors"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
 
+	"github.com/go-chi/httplog/v2"
+	"github.com/vadimbarashkov/url-shortener/internal/config"
+	"github.com/vadimbarashkov/url-shortener/internal/database/postgres"
+	"github.com/vadimbarashkov/url-shortener/internal/service"
+	"golang.org/x/sync/errgroup"
+
+	myhttp "github.com/vadimbarashkov/url-shortener/internal/api/http"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	if err := run(ctx); err != nil {
+		panic(err)
+	}
+}
+
+func run(ctx context.Context) error {
+	cfg, err := config.Load(os.Getenv("CONFIG_PATH"))
+	if err != nil {
+		return err
+	}
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	db, err := postgres.New(cfg.Postgres)
+	if err != nil {
+		return err
+	}
+	g.Go(func() error {
+		<-ctx.Done()
+		return db.Close()
+	})
+
+	urlRepo := postgres.NewURLRepository(db)
+	urlSvc := service.NewURLService(urlRepo, cfg.ShortCodeLength)
+
+	r := myhttp.NewRouter(httplog.NewLogger(""), urlSvc)
+
+	server := &http.Server{
+		Addr:           cfg.Server.Addr(),
+		Handler:        r,
+		ReadTimeout:    cfg.Server.ReadTimeout,
+		WriteTimeout:   cfg.Server.WriteTimeout,
+		IdleTimeout:    cfg.Server.IdleTimeout,
+		MaxHeaderBytes: 1 << 20,
+		BaseContext: func(_ net.Listener) context.Context {
+			return ctx
+		},
+	}
+
+	g.Go(func() error {
+		err := server.ListenAndServeTLS(cfg.Server.CertFile, cfg.Server.KeyFile)
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			return err
+		}
+		return nil
+	})
+
+	g.Go(func() error {
+		<-ctx.Done()
+		return server.Shutdown(context.Background())
+	})
+
+	return g.Wait()
 }
