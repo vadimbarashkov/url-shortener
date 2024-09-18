2696036f4a7b33f4f062d3d82dcd2a65a2449c61
Vadim Barashkov <vadimdominik2005@gmail.com>
2024-09-18 15:19:07 +0300

    feat: write config package
    Write config package that load config from yaml file.

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,65 +2,89 @@ package config
 
 import (
 	"fmt"
+	"os"
 	"time"
 
-	"github.com/caarlos0/env/v11"
-	"github.com/joho/godotenv"
+	"gopkg.in/yaml.v3"
 )
 
-// Config represents the project configuration.
+const (
+	EnvDev   = "dev"
+	EnvStage = "stage"
+	EnvProd  = "prod"
+)
+
 type Config struct {
-	Env             string `env:"ENV" envDefault:"dev"`
-	ShortCodeLength int    `env:"SHORT_CODE_LENGTH" envDefault:"7"`
-	Server          `envPrefix:"SERVER_"`
-	Postgres        `envPrefix:"POSTGRES_"`
+	Env             string `yaml:"env"`
+	ShortCodeLength int    `yaml:"short_code_length"`
+	HTTPServer      `yaml:"http_server"`
+	Postgres        `yaml:"postgres"`
 }
 
-// Load loads the project configuration from the specified .env file path.
-// It returns a pointer to Config struct or an error.
+type HTTPServer struct {
+	Port           int           `yaml:"port"`
+	ReadTimeout    time.Duration `yaml:"read_timeout"`
+	WriteTimeout   time.Duration `yaml:"write_timeout"`
+	IdleTimeout    time.Duration `yaml:"idle_timeout"`
+	MaxHeaderBytes int           `yaml:"max_header_bytes"`
+	CertFile       string        `yaml:"cert_file"`
+	KeyFile        string        `yaml:"key_file"`
+}
+
+var defaultHTTPServer = HTTPServer{
+	Port:           8080,
+	ReadTimeout:    time.Second,
+	WriteTimeout:   2 * time.Second,
+	IdleTimeout:    time.Minute,
+	MaxHeaderBytes: 1 << 20,
+}
+
+func (s *HTTPServer) Addr() string {
+	return fmt.Sprintf(":%d", s.Port)
+}
+
+type Postgres struct {
+	User     string `yaml:"user"`
+	Password string `yaml:"password"`
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	DB       string `yaml:"db"`
+	SSLMode  string `yaml:"sslmode"`
+}
+
+var defaultPostgres = Postgres{
+	Host:    "localhost",
+	Port:    5432,
+	SSLMode: "disable",
+}
+
+func (p *Postgres) DSN() string {
+	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		p.User, p.Password, p.Host, p.Port, p.DB, p.SSLMode)
+}
+
 func Load(path string) (*Config, error) {
 	const op = "config.Load"
 
-	if err := godotenv.Load(path); err != nil {
-		return nil, fmt.Errorf("%s: failed to load .env file: %w", op, err)
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("%s: failed to open config file: %w", op, err)
 	}
+	defer f.Close()
 
 	var cfg Config
+	setDefaults(&cfg)
 
-	if err := env.Parse(&cfg); err != nil {
-		return nil, fmt.Errorf("%s: failed to parse envs: %w", op, err)
+	if err := yaml.NewDecoder(f).Decode(&cfg); err != nil {
+		return nil, fmt.Errorf("%s: failed to decode config file: %w", op, err)
 	}
 
 	return &cfg, nil
 }
 
-// Server represents the server configuration.
-type Server struct {
-	Port         int           `env:"PORT" envDefault:"8443"`
-	ReadTimeout  time.Duration `env:"READ_TIMEOUT" envDefault:"1s"`
-	WriteTimeout time.Duration `env:"WRITE_TIMEOUT" envDefault:"2s"`
-	IdleTimeout  time.Duration `env:"IDLE_TIMEOUT" envDefault:"1m"`
-	CertFile     string        `env:"CERT_FILE"`
-	KeyFile      string        `env:"KEY_FILE"`
-}
-
-// Addr returns the server address in the format ":<port>".
-func (s *Server) Addr() string {
-	return fmt.Sprintf(":%d", s.Port)
-}
-
-// Postgres represents the PostgreSQL configuration.
-type Postgres struct {
-	User     string `env:"USER" envDefault:"postgres"`
-	Password string `env:"PASSWORD"`
-	Host     string `env:"HOST" envDefault:"localhost"`
-	Port     int    `env:"PORT" envDefault:"5432"`
-	DB       string `env:"DB"`
-	SSLMode  string `env:"SSLMODE" envDefault:"disable"`
-}
-
-// DSN returns the Data Source Name (DSN) for connecting to the PostgreSQL.
-func (p *Postgres) DSN() string {
-	return fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
-		p.User, p.Password, p.Host, p.Port, p.DB, p.SSLMode)
+func setDefaults(cfg *Config) {
+	cfg.Env = EnvDev
+	cfg.ShortCodeLength = 7
+	cfg.HTTPServer = defaultHTTPServer
+	cfg.Postgres = defaultPostgres
 }
